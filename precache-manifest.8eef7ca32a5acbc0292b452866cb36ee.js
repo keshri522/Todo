4946d290f69cd90cbfdf8e3b0a6577ed37c71078
keshri522 <rkeshri522@gmail.com>
2023-02-21 01:06:47 +0530

    Updates

---
+++ precache-manifest.8eef7ca32a5acbc0292b452866cb36ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4f4eb3913472659b5a1",
+    "url": "/Todo/static/css/main.7ab7552c.chunk.css"
+  },
+  {
+    "revision": "c4f4eb3913472659b5a1",
+    "url": "/Todo/static/js/main.c4f4eb39.chunk.js"
+  },
+  {
+    "revision": "e5846861b89810b99e9d",
+    "url": "/Todo/static/js/1.e5846861.chunk.js"
+  },
+  {
+    "revision": "ebce75d27e5a84e9d4c0",
+    "url": "/Todo/static/css/2.02181af5.chunk.css"
+  },
+  {
+    "revision": "ebce75d27e5a84e9d4c0",
+    "url": "/Todo/static/js/2.ebce75d2.chunk.js"
+  },
+  {
+    "revision": "d9427863977869c3cebd",
+    "url": "/Todo/static/js/runtime~main.d9427863.js"
+  },
+  {
+    "revision": "fef5713befcacf419f892f50facdf049",
+    "url": "/Todo/index.html"
+  }
+];
